e147b59a7b65406dbed11c8d0965e9e61f5804cf
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-04-27 11:28:21 +0900

    add attribute operators

--- core/src/attribute.lisp
+++ core/src/attribute.lisp
@@ -1,6 +1,15 @@
 (in-package :ter)
 
-(defun tx-make-attribute (graph name data-type)
-  (shinra:tx-make-vertex graph 'attribute
-                         `((name ,name)
-                           (data-type ,data-type))))
+(defun find-attribute (graph)
+  (shinra:find-vertex graph 'attribute))
+
+(defun get-attribute (graph &key code)
+  (car (shinra:find-vertex graph 'attribute :slot 'code :value code)))
+
+(defun tx-make-attribute (graph code name data-type)
+  (or (get-attribute graph :code code)
+      (shinra:tx-make-vertex graph
+                             'attribute
+                             `((code ,code)
+                               (name ,name)
+                               (data-type ,data-type)))))

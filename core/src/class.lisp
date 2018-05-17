11e717f201debeaddfe519e0b4257be77e89a96d
yanqirenshi <yanqirenshi@gmail.com>
2018-05-17 15:05:19 +0900

    add class of attribute-entity

--- core/src/class.lisp
+++ core/src/class.lisp
@@ -1,5 +1,8 @@
 (in-package :ter)
 
+;;;;;
+;;;;; attribute
+;;;;;
 (defclass attribute (shinra:shin)
   ((code :accessor code :initarg :code :initform nil)
    (name :accessor name :initarg :name :initform nil)
@@ -12,6 +15,9 @@
     (jojo:write-key-value "data_type" (slot-value obj 'data-type))
     (jojo:write-key-value "_class" 'attribute)))
 
+;;;;;
+;;;;; entity
+;;;;;
 (defclass entity (shinra:shin)
   ((code       :accessor code       :initarg :code       :initform nil)
    (name       :accessor name       :initarg :name       :initform nil)
@@ -24,3 +30,18 @@
     (when-let (slot-value obj'attributes)
       (jojo:write-key-value "attributes" (slot-value obj'attributes)))
     (jojo:write-key-value "_class" 'entity)))
+
+;;;;;
+;;;;; attribute-entity
+;;;;;
+(defclass attribute-entity (shinra:shin)
+  ((code :accessor code :initarg :code :initform nil)
+   (name :accessor name :initarg :name :initform nil)
+   (data-type :accessor data-type :initarg :data-type :initform nil)))
+
+(defmethod jojo:%to-json ((obj attribute-entity))
+  (jojo:with-object
+    (jojo:write-key-value "code" (slot-value obj 'code))
+    (jojo:write-key-value "name" (slot-value obj 'name))
+    (jojo:write-key-value "data_type" (slot-value obj 'data-type))
+    (jojo:write-key-value "_class" 'attribute-entity)))

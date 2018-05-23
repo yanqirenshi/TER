7601563a0f79c3c2fab8f37e26233c8ecbbc6deb
yanqirenshi <yanqirenshi@gmail.com>
2018-05-23 10:21:31 +0900

    add ter's class

---
+++ core/src/class/ter.lisp
@@ -0,0 +1,32 @@
+(in-package :ter)
+
+;;;;;
+;;;;; Entity
+;;;;;
+(defclass resource (shinra:shin) ())
+(defclass event (shinra:shin) ())
+(defclass correspondence (shinra:shin) ())
+(defclass comparative (shinra:shin) ())
+(defclass recursion (shinra:shin) ())
+
+
+;;;;;
+;;;;; Identifier
+;;;;;
+;; identifier
+;; identifier-instance
+(defclass ter-identifier (shinra:shin) ())
+(defclass ter-identifier-instance)
+
+
+;;;;;
+;;;;; Attribute
+;;;;;
+(defclass ter-attribute (shinra:shin) ())
+(defclass ter-attribute-instance)
+
+
+;;;;;
+;;;;; Relationship
+;;;;;
+(defclass ter-edge (shinra:ra) ())

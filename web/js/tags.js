5598a3de39f53bfd1f5c408abe94a6e16ce78ff0
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-05-23 09:30:07 +0900

    add page tag size and svg in page-01

--- web/js/tags.js
+++ web/js/tags.js
@@ -1,4 +1,4 @@
-riot.tag2('app', '<page01></page01> <page02></page02> <page03></page03>', 'app stage,[data-is="app"] stage{ width: 100vw; height: 100vh; overflow: hidden; display: block; }', '', function(opts) {
+riot.tag2('app', '<page01 class="page"></page01> <page02 class="page"></page02> <page03 class="page"></page03>', 'app > .page { width: 100vw; height: 100vh; overflow: hidden; display: block; }', '', function(opts) {
      window.addEventListener('resize', (event) => {
          this.update();
      });
@@ -8,7 +8,20 @@ riot.tag2('app', '<page01></page01> <page02></page02> <page03></page03>', 'app s
      });
 });
 
-riot.tag2('page01', '', '', '', function(opts) {
+riot.tag2('page01', '<svg ref="svg-tag"></svg>', '', 'ref="self"', function(opts) {
+     this.d3svg = null;
+
+     this.on('mount', () => {
+         this.d3svg = new D3Svg({
+             d3: d3,
+             svg: d3.select("stage svg"),
+             x: 0,
+             y: 0,
+             w: this.refs.self.clientWidth,
+             h: this.refs.self.clientHeight,
+             scale: 1
+         });
+     });
 });
 
 riot.tag2('page02', '', '', '', function(opts) {

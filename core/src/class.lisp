0e6cf3e52c423c5f90850ffcf36bfc6d7bc15ba4
yanqirenshi <yanqirenshi@gmail.com>
2018-05-23 10:05:56 +0900

    STORE への er データの保管までが完了。

--- core/src/class.lisp
+++ core/src/class.lisp
@@ -10,26 +10,11 @@
 
 (defmethod jojo:%to-json ((obj attribute))
   (jojo:with-object
+    (jojo:write-key-value "_id"  (slot-value obj 'up:%id))
     (jojo:write-key-value "code" (slot-value obj 'code))
     (jojo:write-key-value "name" (slot-value obj 'name))
     (jojo:write-key-value "data_type" (slot-value obj 'data-type))
     (jojo:write-key-value "_class" 'attribute)))
-
-;;;;;
-;;;;; entity
-;;;;;
-(defclass entity (shinra:shin)
-  ((code       :accessor code       :initarg :code       :initform nil)
-   (name       :accessor name       :initarg :name       :initform nil)
-   (attributes :accessor attributes :initarg :attributes :initform nil)))
-
-(defmethod jojo:%to-json ((obj entity))
-  (jojo:with-object
-    (jojo:write-key-value "code" (slot-value obj 'code))
-    (jojo:write-key-value "name" (slot-value obj 'name))
-    (when-let (slot-value obj'attributes)
-      (jojo:write-key-value "attributes" (slot-value obj'attributes)))
-    (jojo:write-key-value "_class" 'entity)))
 
 ;;;;;
 ;;;;; attribute-entity
@@ -41,7 +26,25 @@
 
 (defmethod jojo:%to-json ((obj attribute-entity))
   (jojo:with-object
+    (jojo:write-key-value "_id"  (slot-value obj 'up:%id))
     (jojo:write-key-value "code" (slot-value obj 'code))
     (jojo:write-key-value "name" (slot-value obj 'name))
     (jojo:write-key-value "data_type" (slot-value obj 'data-type))
     (jojo:write-key-value "_class" 'attribute-entity)))
+
+;;;;;
+;;;;; entity
+;;;;;
+(defclass entity (shinra:shin)
+  ((code       :accessor code       :initarg :code       :initform nil)
+   (name       :accessor name       :initarg :name       :initform nil)
+   (attributes :accessor attributes :initarg :attributes :initform nil)))
+
+(defmethod jojo:%to-json ((obj entity))
+  (jojo:with-object
+    (jojo:write-key-value "_id"  (slot-value obj 'up:%id))
+    (jojo:write-key-value "code" (slot-value obj 'code))
+    (jojo:write-key-value "name" (slot-value obj 'name))
+    (when-let (slot-value obj'attributes)
+      (jojo:write-key-value "attributes" (slot-value obj'attributes)))
+    (jojo:write-key-value "_class" 'entity)))

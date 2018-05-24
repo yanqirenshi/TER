b6821c2e980d2f909880861fb3abe87c77a6e8dd
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-05-24 09:40:33 +0900

    add class of Table

--- web/js/tags.js
+++ web/js/tags.js
@@ -11,6 +11,33 @@ riot.tag2('app', '<page01 class="page"></page01> <page02 class="page"></page02> 
 riot.tag2('page01', '<svg></svg>', '', 'ref="self"', function(opts) {
      this.d3svg = null;
 
+     this.random = (type) => {
+         let min;
+         let max;
+
+         if (type=='x') {
+             min = 11;
+             max = this.refs.self.clientWidth - 11;
+         }
+         if (type=='y') {
+             min = 11;
+             max = this.refs.self.clientHeight - 11;
+         }
+         return Math.floor( Math.random() * (max + 1 - min) ) + min ;
+     }
+
+     this.drawTables = () => {
+         let table = new Table();
+
+         let data = STORE.state().get('er').tables.list;
+         table.draw(this.d3svg, data);
+     };
+
+     STORE.subscribe((action) => {
+         if(action.type=='FETCHED-ER-TABLES')
+             this.drawTables();
+     });
+
      this.on('mount', () => {
          this.d3svg = new D3Svg({
              d3: d3,
--- web/tags/page01.tag
+++ web/tags/page01.tag
@@ -5,6 +5,33 @@
     <script>
      this.d3svg = null;
 
+     this.random = (type) => {
+         let min;
+         let max;
+
+         if (type=='x') {
+             min = 11;
+             max = this.refs.self.clientWidth - 11;
+         }
+         if (type=='y') {
+             min = 11;
+             max = this.refs.self.clientHeight - 11;
+         }
+         return Math.floor( Math.random() * (max + 1 - min) ) + min ;
+     }
+
+     this.drawTables = () => {
+         let table = new Table();
+
+         let data = STORE.state().get('er').tables.list;
+         table.draw(this.d3svg, data);
+     };
+
+     STORE.subscribe((action) => {
+         if(action.type=='FETCHED-ER-TABLES')
+             this.drawTables();
+     });
+
      this.on('mount', () => {
          this.d3svg = new D3Svg({
              d3: d3,

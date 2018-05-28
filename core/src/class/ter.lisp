73de9d01002753d3232f7672be62dd9038fa0c43
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-05-28 10:26:11 +0900

    add class of port

--- core/src/class/ter.lisp
+++ core/src/class/ter.lisp
@@ -1,5 +1,17 @@
 (in-package :ter)
 
+(defclass port (shinra:shin rsc point) ())
+
+(defmethod jojo:%to-json ((obj port))
+  (jojo:with-object
+    (jojo:write-key-value "_id"  (slot-value obj 'up:%id))
+    ;; point
+    (jojo:write-key-value "x" (slot-value obj 'x))
+    (jojo:write-key-value "y" (slot-value obj 'y))
+    (jojo:write-key-value "z" (slot-value obj 'z))
+    (jojo:write-key-value "_class" "port")))
+
+
 ;;;;;
 ;;;;; Entity
 ;;;;;

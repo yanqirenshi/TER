c8d192b49e0e114e1fc45f99639273a37aa95bdf
yanqirenshi <yanqirenshi@gmail.com>
2018-06-18 10:07:20 +0900

    schema ごとに graph を追加

--- core/src/er/schema.lisp
+++ core/src/er/schema.lisp
@@ -3,8 +3,8 @@
 (defun find-schema (graph)
   (shinra:find-vertex graph 'schema))
 
-(defun get-schema (graph)
-  (car (shinra:find-vertex graph 'schema)))
+(defun get-schema (graph &key code)
+  (car (shinra:find-vertex graph 'schema :slot 'code :value code)))
 
 (defgeneric tx-make-schema (graph code &key name description)
   (:method (graph code &key name description)
@@ -13,3 +13,21 @@
                            `((code ,code)
                              (name ,name )
                              (description ,description)))))
+
+(defvar *er-schema-graphs* (make-hash-table))
+
+(defgeneric start-schema-graph (schema)
+  (:method ((schema schema))
+    (let ((code (code schema))
+          (store-dir (store-directory schema)))
+      (when (gethash code *er-schema-graphs*)
+        (error "xxx"))
+      (setf (gethash code *er-schema-graphs*)
+            (shinra:make-banshou 'shinra:banshou store-dir)))))
+
+(defgeneric snapshot-schema-graph (schema)
+  (:method ((schema schema))
+    (let ((code (code schema)))
+      (unless (gethash code *er-schema-graphs*)
+        (error "xxx"))
+      (up:snapshot (gethash code *er-schema-graphs*)))))

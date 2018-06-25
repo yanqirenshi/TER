d2ff7767f74ea459dbabc72d4d86ef097ff3f5fa
yanqirenshi <yanqirenshi@gmail.com>
2018-06-25 11:55:16 +0900

    import fk もうすこし。 port まで取得。

--- core/src/er/table.lisp
+++ core/src/er/table.lisp
@@ -3,13 +3,16 @@
 (defun find-table (graph)
   (find-vertex graph 'table))
 
-(defun get-table (graph &key code (with-columns t))
-  (let ((table (car (find-vertex graph 'table :slot 'code :value code))))
-    (when table
-      (when with-columns
-        (setf (columns table)
-              (find-table-columns graph table)))
-      table)))
+(defgeneric get-table (graph &key code with-columns)
+  (:method ((schema schema) &key code (with-columns t))
+    (get-table (get-schema-graph schema) :code code :with-columns with-columns))
+  (:method ((graph shinra:banshou) &key code (with-columns t))
+    (let ((table (car (find-vertex graph 'table :slot 'code :value code))))
+      (when table
+        (when with-columns
+          (setf (columns table)
+                (find-table-column-instances graph table)))
+        table))))
 
 (defun tx-make-table (graph code name)
   (or (get-table graph :code code)
@@ -22,18 +25,31 @@
                         (w 222)
                         (h 333)))))
 
-(defun find-table-columns (graph table)
-  (shinra:find-r-vertex graph 'edge-er
-                        :from table
-                        :vertex-class 'column-instance
-                        :edge-type :have))
+(defgeneric find-table-column-instances (graph table)
+  (:method ((schema schema) (table table))
+    (find-table-column-instances (get-schema-graph schema) table))
+  (:method ((graph shinra:banshou) (table table))
+    (shinra:find-r-vertex graph 'edge-er
+                          :from table
+                          :vertex-class 'column-instance
+                          :edge-type :have)))
 
 (defun get-table-column-instance (graph table &key data code)
   (when (and graph table (or data code))
     (find-if #'(lambda (d)
                  (eq (code d)
                      (cond (data (make-column-instance-code table
-                                                            (getf data :name)
-                                                            (make-data-type data)))
+                                                            (getf data :name)))
                            (code code))))
-             (find-table-columns graph table))))
+             (find-table-column-instances graph table))))
+
+(defgeneric get-table-column-instances-port (graph table column-code)
+  (:method ((schema schema) (table table) (column-code symbol))
+    (get-table-column-instances-port (get-schema-graph schema) table column-code))
+  (:method ((graph shinra:banshou) (table table) (column-code symbol))
+    (let ((column-instance (get-column-instance graph :code column-code)))
+      (or (first (shinra:find-r-vertex graph 'edge-er
+                                       :from column-instance
+                                       :vertex-class 'port-er
+                                       :edge-type :have))
+          (add-port-er graph column-instance)))))
--- core/src/importer/schema.rb.lisp
+++ core/src/importer/schema.rb.lisp
@@ -35,8 +35,8 @@
 (defun make-column-code (name data-type)
   (make-code name "@" data-type))
 
-(defun make-column-instance-code (table name data-type)
-  (make-code (symbol-name (code table)) "." name "@" data-type))
+(defun make-column-instance-code (table name)
+  (make-code (symbol-name (code table)) "." name))
 
 (defun make-column-column-type (name)
   (let ((attribute-names '("created_at" "creator_id" "updated_at" "updater_id")))
@@ -64,7 +64,7 @@
 (defun tx-import-column-instance (graph table data)
   (let* ((name (getf data :name))
          (data-type (make-data-type data))
-         (code (make-column-instance-code table name data-type)))
+         (code (make-column-instance-code table name)))
     (tx-make-column-instance graph code name
                              data-type
                              (make-column-column-type name))))
@@ -89,7 +89,7 @@
 (defun %tx-import-foreign-key (graph from-table plist)
   (let* ((to-table-code (make-table-code (getf (getf plist :foreign-key) :references)))
          (to-table (get-table graph :code to-table-code))
-         (to-code (make-column-instance-code to-table "id" "integer")))
+         (to-code (make-column-instance-code to-table "id")))
     (values (get-table-column-instance graph from-table :data plist)
             (get-table-column-instance graph to-table :code to-code))))
 
@@ -107,7 +107,7 @@
 (defun get-id-column-instance (graph table)
   (find-if #'(lambda (attr)
                (eq (code attr)
-                   (make-column-instance-code table "id" "integer")))
+                   (make-column-instance-code table "id")))
            (shinra:find-r-vertex graph 'edge-er
                                  :from table)))
 
@@ -136,8 +136,70 @@
       (tx-import-all-foreign-keys graph plists)
       tables)))
 
+(defun get-value-string (value)
+  (multiple-value-bind (ret arr)
+      (cl-ppcre:scan-to-strings  "^\"(.*)\"$" (string-trim '(#\Space #\Tab #\Newline) value))
+    (unless ret (warn "~S がパース出来ませんでした。" value))
+    (if ret (aref arr 0) nil)))
+
+(defun str2keyword (str)
+  (alexandria:make-keyword (string-upcase str)))
+
+(defun parse-foreign-key-options (options)
+  (when-let ((option (car options)))
+    (multiple-value-bind (ret arr)
+        (cl-ppcre:scan-to-strings  "^(\\S+):\\s+(\\S+)$" (string-trim '(#\Space #\Tab #\Newline) option))
+      (unless ret (warn "~S がパース出来ませんでした。" option))
+      (if (not ret)
+          (parse-foreign-key-options (cdr options))
+          (nconc (list (str2keyword (aref arr 0))
+                       (get-value-string (aref arr 1)))
+                 (parse-foreign-key-options (cdr options)))))))
+
+
+
+(defun parse-foreign-key-line-column-code (type table-name options)
+  (let ((options-column (getf options :column))
+        (options-primary-key (getf options :primary_key)))
+    (cond ((eq :from type)
+           (if (and options-column options-primary-key)
+               options-column
+               "id"))
+          ((eq :to type)
+           (or options-column
+               (concatenate 'string (cl-inflector:singular-of table-name) "_id"))))))
+
+(defun parse-foreign-key-line (graph line)
+  (multiple-value-bind (ret arr)
+      (cl-ppcre:scan-to-strings  "^add_foreign_key\\s+(.*)$" (string-trim '(#\Space #\Tab #\Newline) line))
+    (unless ret
+      (warn "~S がパース出来ませんでした。" line))
+    (let* ((items (split-sequence:split-sequence #\, (aref arr 0)))
+           (table-name-to   (get-value-string (first items)))
+           (table-name-from (get-value-string (second items)))
+           (options (parse-foreign-key-options (cddr items)))
+           (table-from (get-table graph :code (str2keyword table-name-from)))
+           (table-to (get-table graph :code (str2keyword table-name-to)))
+           (column-code-from (make-column-instance-code table-from (parse-foreign-key-line-column-code :from table-name-from options)))
+           (column-code-to   (make-column-instance-code table-to   (parse-foreign-key-line-column-code :to   table-name-from options))))
+      ;;
+      ;; TODO: これで関係をさくせいする。
+      ;;
+      (list :table-from      table-from
+            :table-to        table-to
+            :table-from-port (get-table-column-instances-port graph table-from column-code-from)
+            :table-to-port   (get-table-column-instances-port graph table-to   column-code-to)))))
+
+(defun import-keys (graph plists)
+  (when-let ((plist (car plists)))
+    (if (eq :add-foreign-key (getf plist :type))
+        (cons (parse-foreign-key-line graph (getf plist :line))
+              (import-keys graph (cdr plists)))
+        (import-keys graph (cdr plists)))))
+
 (defun import-schema.rb (graph pathname)
   (multiple-value-bind (tables keys)
       (ter.parser:parse-schema.rb pathname)
-    (print keys)
+    (plist-printer:pprints (import-keys graph keys)
+                           '( :table-from :table-to :table-from-port :table-to-port :options))
     (%import-schema.rb graph tables)))

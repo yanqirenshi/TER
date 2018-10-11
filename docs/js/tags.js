85f132db4dea9b6ecb0b3e8969fc241c3f55faee
yanqirenshi <yanqirenshi@gmail.com>
2018-10-11 16:12:58 +0900

    add datamodel in docs/

--- docs/js/tags.js
+++ docs/js/tags.js
@@ -203,16 +203,59 @@ riot.tag2('core_page_root', '<section-header title="TER: Core"></section-header>
      };
 });
 
-riot.tag2('core_page_tab_classes', '', '', '', function(opts) {
+riot.tag2('core_page_tab_classes', '<section class="section"> <div class="container"> <h1 class="title is-4">List</h1> <h2 class="subtitle"></h2> <section class="section"> <div class="container"> <h1 class="title is-5">TER</h1> <h2 class="subtitle"></h2> <div class="contents"> <table class="table"> <thead> <tr> <th>Name</th> <th>Description</th> </tr> </thead> <tbody> <tr each="{getClasses(\'ter\')}"> <td>{name.toUpperCase()}</td> <td>{description}</td> </tr> </tbody> </table> </div> </div> </section> <section class="section"> <div class="container"> <h1 class="title is-5">TER</h1> <h2 class="subtitle"></h2> <div class="contents"> <table class="table"> <thead> <tr> <th>Name</th> <th>Description</th> </tr> </thead> <tbody> <tr each="{getClasses(\'er\')}"> <td>{name.toUpperCase()}</td> <td>{description}</td> </tr> </tbody> </table> </div> </div> </section> </div> </section>', '', '', function(opts) {
+     this.getClasses = (group) => {
+         return this.classes.filter((d) => {
+             return d.group == group;
+         });
+     };
+     this.classes = [
+         { group: 'common', package: '', name: 'rsc',                 parent: ''                    },
+         { group: 'common', package: '', name: 'rect',                parent: ''                    },
+         { group: 'common', package: '', name: 'port',                parent: ''                    },
+         { group: 'common', package: '', name: 'point',               parent: ''                    },
+
+         { group: 'ter',    package: '', name: 'resource',            parent: 'shin rsc point rect' },
+         { group: 'ter',    package: '', name: 'event',               parent: 'shin rsc point rect' },
+         { group: 'ter',    package: '', name: 'comparative',         parent: 'shin rsc point rect' },
+         { group: 'ter',    package: '', name: 'correspondence',      parent: 'shin rsc point rect' },
+         { group: 'ter',    package: '', name: 'recursion',           parent: 'shin rsc point rect' },
+         { group: 'ter',    package: '', name: 'identifier',          parent: 'shin'                },
+         { group: 'ter',    package: '', name: 'identifier-instance', parent: 'shin'                },
+         { group: 'ter',    package: '', name: 'attribute',           parent: 'shin'                },
+         { group: 'ter',    package: '', name: 'attribute-instance',  parent: 'shin'                },
+         { group: 'ter',    package: '', name: 'port-ter',            parent: 'shin rsc point'      },
+         { group: 'ter',    package: '', name: 'edge-ter',            parent: 'ra'                  },
+         { group: 'ter',    package: '', name: 'campus',              parent: 'shin rsc'            },
+
+         { group: 'er',     package: '', name: 'table',               parent: 'shin rsc point rect' },
+         { group: 'er',     package: '', name: 'column',              parent: 'shin rsc'            },
+         { group: 'er',     package: '', name: 'column-instance',     parent: 'shin rsc'            },
+         { group: 'er',     package: '', name: 'port-er',             parent: 'shin rsc port'       },
+         { group: 'er',     package: '', name: 'port-er-in',          parent: 'port-er'             },
+         { group: 'er',     package: '', name: 'port-er-out',         parent: 'port-er'             },
+         { group: 'er',     package: '', name: 'edge-er',             parent: 'ra'                  },
+
+         { group: 'mapper', package: '', name: 'edge-map',            parent: 'ra'                  },
+         { group: 'base',   package: '', name: 'camera',              parent: 'shin rsc'            },
+         { group: 'base',   package: '', name: 'edge',                parent: 'ra'                  },
+         { group: 'base',   package: '', name: 'schema',              parent: 'shin rsc'            },
+     ];
 });
 
-riot.tag2('core_page_tab_datamodels', '', '', '', function(opts) {
+riot.tag2('core_page_tab_datamodels', '<section class="section"> <div class="container"> <h1 class="title is-4">TER図</h1> <h2 class="subtitle"></h2> <div class="contents"> <p><pre>\n  entity\n    ^\n    |\n    +-------+---------+-------------+-------------+\n    |       |         |             |             |\nresource  event  comparative  correspondence  recursion\n\n\n  identifier ------1:n------> identifier-instance\n  attribute  ------1:n------> attribute-instance\n\n\n  entity ------1:n------> identifier-instance\n  entity ------1:n------> attribute-instance\n\n\n  identifier-instance ------1:n------> port-ter-in\n  identifier-instance ------1:n------> port-ter-out\n\n\n  port-ter-in ------1:n------> edge-ter ------1:n------> port-ter-out</pre></p> </div> </div> </section> <section class="section"> <div class="container"> <h1 class="title is-4">ER図</h1> <h2 class="subtitle"></h2> <div class="contents"> <p><pre>\n  table  ------1:n------> column-instance\n\n\n  column ------1:n------> column-instance\n\n\n  column-instance -------1:1------> port-er-in\n  column-instance -------1:1------> port-er-out\n\n\n  port-er-in ------1:1------> edge-er ------1:1------> port-er-out</pre></p> </div> </div> </section>', '', '', function(opts) {
 });
 
 riot.tag2('core_page_tab_operators', '', '', '', function(opts) {
 });
 
-riot.tag2('core_page_tab_packages', '', '', '', function(opts) {
+riot.tag2('core_page_tab_packages', '<section class="section"> <div class="container"> <h1 class="title is-4">List</h1> <h2 class="subtitle"></h2> <div class="contents"> <table class="table"> <thead> <tr> <th>Name</th> <th>Description</th> </tr> </thead> <tbody> <tr each="{packages}"> <td>{name.toUpperCase()}</td> <td>{description}</td> </tr> </tbody> </table> </div> </div> </section>', '', '', function(opts) {
+     this.packages = [
+         { code: 'ter',        name: 'ter',        description: '' },
+         { code: 'ter.parser', name: 'ter.parser', description: '' },
+         { code: 'ter.db',     name: 'ter.db',     description: '' },
+         { code: 'ter-test',   name: 'ter-test',   description: '' },
+     ]
 });
 
 riot.tag2('core_page_tab_readme', '', '', '', function(opts) {
--- docs/tags/core/core_page_tab_packages.tag
+++ docs/tags/core/core_page_tab_packages.tag
@@ -1,2 +1,34 @@
 <core_page_tab_packages>
+    <section class="section">
+        <div class="container">
+            <h1 class="title is-4">List</h1>
+            <h2 class="subtitle"></h2>
+            <div class="contents">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr each={packages}>
+                            <td>{name.toUpperCase()}</td>
+                            <td>{description}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+
+
+    <script>
+     this.packages = [
+         { code: 'ter',        name: 'ter',        description: '' },
+         { code: 'ter.parser', name: 'ter.parser', description: '' },
+         { code: 'ter.db',     name: 'ter.db',     description: '' },
+         { code: 'ter-test',   name: 'ter-test',   description: '' },
+     ]
+    </script>
 </core_page_tab_packages>

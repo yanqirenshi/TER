6eec66baa9119033e4b7bc2f0716b4e8762c7234
yanqirenshi <yanqirenshi@gmail.com>
2019-01-02 11:10:01 +0900

    add api for attr id edge-ter

--- core/src/classes/ter/attribute.lisp
+++ core/src/classes/ter/attribute.lisp
@@ -4,3 +4,12 @@
 
 (defclass attribute-instance (shinra:shin rsc)
  ((data-type :accessor data-type :initarg :data-type :initform nil)))
+
+(defmethod jojo:%to-json ((obj attribute-instance))
+  (jojo:with-object
+    (jojo:write-key-value "_id"         (slot-value obj 'up:%id))
+    (jojo:write-key-value "code"        (slot-value obj 'code))
+    (jojo:write-key-value "name"        (slot-value obj 'name))
+    (jojo:write-key-value "data-type"   (slot-value obj 'data-type))
+    (jojo:write-key-value "description" (slot-value obj 'description))
+    (jojo:write-key-value "_class"   "ATTRIBUTE-INSTANCE")))
--- core/src/classes/ter/resource.lisp
+++ core/src/classes/ter/resource.lisp
@@ -4,9 +4,10 @@
 
 (defmethod jojo:%to-json ((obj resource))
   (jojo:with-object
-    (jojo:write-key-value "_id"      (slot-value obj 'up:%id))
-    (jojo:write-key-value "code"     (slot-value obj 'code))
-    (jojo:write-key-value "name"     (slot-value obj 'name))
-    (jojo:write-key-value "position" (slot-value obj 'location))
-    (jojo:write-key-value "size"     (slot-value obj 'size))
+    (jojo:write-key-value "_id"         (slot-value obj 'up:%id))
+    (jojo:write-key-value "code"        (slot-value obj 'code))
+    (jojo:write-key-value "name"        (slot-value obj 'name))
+    (jojo:write-key-value "description" (slot-value obj 'description))
+    (jojo:write-key-value "position"    (slot-value obj 'location))
+    (jojo:write-key-value "size"        (slot-value obj 'size))
     (jojo:write-key-value "_class"   "RESOURCE")))

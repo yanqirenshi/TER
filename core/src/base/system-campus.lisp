17bbb60c097a2b67aba88dfe516503ab67316a17
yanqirenshi <yanqirenshi@gmail.com>
2019-01-31 08:44:49 +0900

    add sysmte to schema and campus

--- core/src/base/system-campus.lisp
+++ core/src/base/system-campus.lisp
@@ -1,1 +1,19 @@
 (in-package :ter)
+
+(defun get-r-system2campus (graph system campus)
+  (find-if #'(lambda (r)
+               (print r)
+               (let ((campus_tmp (getf r :vertex)))
+                 (eq (code campus_tmp) (code campus))))
+           (shinra:find-r graph 'edge :from system
+                                      :edge-type :have-to
+                                      :vertex-class 'campus)))
+
+(defun get-edge-system2campus (graph system campus)
+  (when-let ((r (get-r-system2campus graph system campus)))
+    (getf r :edge)))
+
+(defgeneric tx-make-edge-system2campus (graph system campus)
+  (:method (graph (system system) (campus campus))
+    (assert (null (get-edge-system2campus graph system campus)))
+    (shinra:tx-make-edge graph 'edge system campus :have-to)))
--- core/src/base/system-schema.lisp
+++ core/src/base/system-schema.lisp
@@ -1,1 +1,19 @@
 (in-package :ter)
+
+(defun get-r-system2schema (graph system schema)
+  (find-if #'(lambda (r)
+               (print r)
+               (let ((schema_tmp (getf r :vertex)))
+                 (eq (code schema_tmp) (code schema))))
+           (shinra:find-r graph 'edge :from system
+                                      :edge-type :have-to
+                                      :vertex-class 'schema)))
+
+(defun get-edge-system2schema (graph system schema)
+  (when-let ((r (get-r-system2schema graph system schema)))
+    (getf r :edge)))
+
+(defgeneric tx-make-edge-system2schema (graph system schema)
+  (:method (graph (system system) (schema schema))
+    (assert (null (get-edge-system2schema graph system schema)))
+    (shinra:tx-make-edge graph 'edge system schema :have-to)))

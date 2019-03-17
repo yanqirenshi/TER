92c39152a5c5275fb944f40e950f285daf267561
yanqirenshi <yanqirenshi@gmail.com>
2019-03-18 08:13:16 +0900

    move some methods to classes

--- web/js/Actions.js
+++ web/js/Actions.js
@@ -47,59 +47,21 @@ class Actions extends Vanilla_Redux_Actions {
     /* **************************************************************** *
      *  Fetch Graph
      * **************************************************************** */
-    makeGraphData (list) {
-        let ht = {};
-        for (var i in list) {
-            let data = list[i];
-            ht[data._id] = data;
-        }
-        return {ht: ht, list: list};
-    }
-    makeGraphRData (list) {
-        let ht = {};
-        let ht_from = {};
-        let ht_to = {};
-        for (var i in list) {
-            let data = list[i];
-            let _id = data._id;
-            let from_id = data['from-id'];
-            let to_id = data['to-id'];
-
-            // _id
-            ht[_id] = data;
-
-            // from_id
-            if (!ht_from[from_id])
-                ht_from[from_id] = {};
-            ht_from[from_id][to_id] = data;
-
-            // to_id
-            if (!ht_to[to_id])
-                ht_to[to_id] = {};
-            ht_to[to_id][from_id] = data;
-        }
-
-        return {
-            ht: ht,
-            list: list,
-            from: ht_from,
-            to: ht_to
-        };
-    }
     fetchGraph (mode) {
         API.get('/graph', function (response) {
             STORE.dispatch(this.fetchedGraph(mode, response));
         }.bind(this));
     }
     fetchedGraph (mode, response) {
+        let dm = new ErDataManeger();
+
         return {
             type: 'FETCHED-GRAPH',
             mode: mode,
             data: {
                 graph: {
-                    nodes: this.makeGraphData(response.NODES),
-                    edges: this.makeGraphData(response.EDGES)
-
+                    nodes: dm.makeGraphData(response.NODES),
+                    edges: dm.makeGraphData(response.EDGES),
                 }
             }
         };
@@ -107,106 +69,6 @@ class Actions extends Vanilla_Redux_Actions {
     /* **************************************************************** *
      *  Fetch ER
      * **************************************************************** */
-    fetchEr (schema, mode) {
-        let scheme_code = schema.code.toLowerCase();
-
-        API.get('/er/' + scheme_code, function (response) {
-            STORE.dispatch(this.fetchedEr(mode, response));
-        }.bind(this));
-    }
-    injectTable2ColumnInstances (tables, column_instances, relashonships) {
-        let table_ht = tables.ht;
-        for (var i in column_instances.list) {
-            let column_instance = column_instances.list[i];
-            let to_ht = relashonships.to[column_instance._id];
-
-            for (var k in to_ht)
-                if (to_ht[k]['from-class'] == 'TABLE') {
-                    column_instance._table = to_ht[k];
-
-                    let from_id = to_ht[k]['from-id'];
-                    column_instance._table = table_ht[from_id];
-
-
-                    if (!table_ht[k]._column_instances)
-                        table_ht[k]._column_instances = [];
-
-                    table_ht[k]._column_instances.push(column_instance);
-                }
-        }
-    }
-    injectColumnInstances2Ports (column_instances, ports, relashonships) {
-        let column_instances_ht = column_instances.ht;
-        for (var i in ports.list) {
-            let port = ports.list[i];
-            let to_ht = relashonships.to[port._id];
-
-            for (var k in to_ht)
-                if (to_ht[k]['from-class'] == 'COLUMN-INSTANCE') {
-                    let from_id = to_ht[k]['from-id'];
-                    port._column_instance = column_instances_ht[from_id];
-
-                    if (!port._column_instance._table._ports)
-                        port._column_instance._table._ports = [];
-                    port._column_instance._table._ports.push(port);
-                }
-        }
-    }
-    makeEdges (relashonships, ports) {
-        let ports_ht = ports.ht;
-
-        return relashonships.list.filter((r) => {
-            let test = ((r['from-class']=='PORT-ER-OUT' || r['from-class']=='PORT-ER-IN') &&
-                        (r['to-class']=='PORT-ER-OUT' || r['to-class']=='PORT-ER-IN'));
-            if (test) {
-                let port_from = ports_ht[r['from-id']];
-                let port_to   = ports_ht[r['to-id']];
-
-                r._port_from = port_from;
-                r._port_to   = port_to;
-
-                let table_from = port_from._column_instance._table;
-                let table_to   = port_to._column_instance._table;
-
-                if (!table_from._edges) table_from._edges = [];
-                if (!table_to._edges)   table_to._edges   = [];
-
-                table_from._edges.push(r);
-                table_to._edges.push(r);
-            }
-            return test;
-        });
-    }
-    fetchedEr (mode, response) {
-        let relashonships    = this.makeGraphRData(response.RELASHONSHIPS);
-        let tables           = this.makeGraphData(response.TABLES);
-        let column_instances = this.makeGraphData(response.COLUMN_INSTANCES);
-        let ports            = this.makeGraphData(response.PORTS);
-
-        // inject
-        this.injectTable2ColumnInstances(tables, column_instances, relashonships);
-        this.injectColumnInstances2Ports (column_instances, ports, relashonships);
-
-        // edges
-        let edges            = this.makeGraphData(this.makeEdges(relashonships, ports));
-
-        return {
-            type: 'FETCHED-ER',
-            mode: mode,
-            data: {
-                er: {
-                    tables:           this.makeGraphData(response.TABLES),
-                    columns:          this.makeGraphData(response.COLUMNS),
-                    column_instances: column_instances,
-                    ports:            ports,
-                    relashonships:    relashonships,
-                    edges:            edges,
-                    cameras:          response.CAMERAS
-                }
-            }
-        };
-    }
-    //
     fetchErNodes (schema, mode) {
         let scheme_code = schema.code.toLowerCase();
 
@@ -215,30 +77,14 @@ class Actions extends Vanilla_Redux_Actions {
         }.bind(this));
     }
     fetchedErNodes (mode, response) {
-        let relashonships    = this.makeGraphRData(response.RELASHONSHIPS);
-        let tables           = this.makeGraphData(response.TABLES);
-        let column_instances = this.makeGraphData(response.COLUMN_INSTANCES);
-        let ports            = this.makeGraphData(response.PORTS);
-
-        this.injectTable2ColumnInstances(tables, column_instances, relashonships);
-        this.injectColumnInstances2Ports (column_instances, ports, relashonships);
-
         return {
             type: 'FETCHED-ER-NODES',
             mode: mode,
             data: {
-                er: {
-                    tables:           this.makeGraphData(response.TABLES),
-                    columns:          this.makeGraphData(response.COLUMNS),
-                    column_instances: column_instances,
-                    ports:            ports,
-                    relashonships:    relashonships,
-                    cameras:          response.CAMERAS
-                }
+                er: new ErDataManeger().responseNode2Data(response),
             }
         };
     }
-    //
     fetchErEdges (schema, mode) {
         let scheme_code = schema.code.toLowerCase();
 
@@ -248,10 +94,11 @@ class Actions extends Vanilla_Redux_Actions {
     }
     fetchedErEdges (mode, response) {
         let new_state        = STORE.state().get('er');
-        let relashonships    = new_state.relashonships;
-        let ports            = new_state.ports;
 
-        new_state.edges = this.makeGraphData(this.makeEdges(relashonships, ports));
+        new_state.edges = new ErDataManeger().responseEdge2Data(
+            response,
+            new_state.relashonships,
+            new_state.ports);
 
         if (mode=='FIRST')
             new_state.first_loaded = true;

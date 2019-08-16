d485bd8087ce896682cbd7196f54aa4f36a43ade
yanqirenshi <yanqirenshi@gmail.com>
2019-08-16 14:06:31 +0900

    add classes of subset

--- core/src/classes/ter/resource.lisp
+++ core/src/classes/ter/resource.lisp
@@ -11,3 +11,15 @@
     (jojo:write-key-value "position"    (slot-value obj 'location))
     (jojo:write-key-value "size"        (slot-value obj 'size))
     (jojo:write-key-value "_class"   "RESOURCE")))
+
+(defclass resource-subset (resource) ())
+
+(defmethod jojo:%to-json ((obj resource-subset))
+  (jojo:with-object
+    (jojo:write-key-value "_id"         (slot-value obj 'up:%id))
+    (jojo:write-key-value "code"        (slot-value obj 'code))
+    (jojo:write-key-value "name"        (slot-value obj 'name))
+    (jojo:write-key-value "description" (slot-value obj 'description))
+    (jojo:write-key-value "position"    (slot-value obj 'location))
+    (jojo:write-key-value "size"        (slot-value obj 'size))
+    (jojo:write-key-value "_class"   "RESOURCE-SUBSET")))

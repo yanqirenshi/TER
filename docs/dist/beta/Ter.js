6a6bcfc930983ceee85eb9e9c5d8b8da35b12823
yanqirenshi <yanqirenshi@gmail.com>
2019-10-04 12:00:38 +0900

    copy file to beta

--- docs/dist/beta/Ter.js
+++ docs/dist/beta/Ter.js
@@ -1117,4 +1117,54 @@ class Ter {
 
         this.drawEdgesCore(elements);
     }
+    /* **************************************************************** *
+     *  to Json
+     * **************************************************************** */
+    stateTER2Json (state) {
+        let out = {};
+
+        out.cameras = state.cameras.list;
+        out.entities = state.entities.list;
+
+        let modifyColumns = (obj) => {
+            let new_data = Object.assign({}, obj);
+
+            delete new_data._entity;
+            delete new_data._parent;
+
+            return new_data;
+        };
+
+        out.identifier_instances = state.identifier_instances.list.map(modifyColumns);
+        out.attribute_instances  = state.attribute_instances.list.map(modifyColumns);
+
+        out.relationships = state.relationships.list.map((obj) => {
+            let new_data = Object.assign({}, obj);
+
+            delete new_data._from;
+            delete new_data._to;
+
+            return new_data;
+        });
+
+        out.ports = state.ports.list.map((obj) => {
+            let new_data = Object.assign({}, obj);
+
+            delete new_data._element;
+
+            return new_data;
+        });
+
+        return JSON.stringify(out, null, 3);
+    }
+    downloadJson (name, json) {
+        var blob = new Blob([ json ], {type : 'application/json'});
+
+        var a = document.createElement("a");
+        a.href = URL.createObjectURL(blob);
+        a.target = '_blank';
+        a.download = name + '.' + moment().format('YYYYMMDDHHmmssZZ') + '.json';
+        a.click();
+
+    }
 }
